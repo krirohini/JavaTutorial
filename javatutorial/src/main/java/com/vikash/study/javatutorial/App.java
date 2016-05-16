8b834caf9c54623de860b28a20cadaba88ae2198
Vikash Kumar <vikashkumar@paypal.com>
2016-05-15 18:39:41 -0700

    Binary search prog

---
+++ javatutorial/src/main/java/com/vikash/study/javatutorial/App.java
@@ -0,0 +1,13 @@
+package com.vikash.study.javatutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
